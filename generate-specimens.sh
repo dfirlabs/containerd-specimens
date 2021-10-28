554e65b3cc6389cb1038d401631f6f794dbd2c29
roshan maskey <roshanmaskey@gmail.com>
2021-10-28 17:43:37 +1100

    Fixed missing line terminator in generate-specimens.sh

--- generate-specimens.sh
+++ generate-specimens.sh
@@ -15,30 +15,30 @@ EXIT_FAILURE=1;
 #
 display_message()
 {
-    local EXIT_STATUS=$1
-    local MESSAGE="$2"
+    local EXIT_STATUS=$1;
+    local MESSAGE="$2";
 
-    local MAX_SIZE=90
-    local padding=""
-    local result=""
+    local MAX_SIZE=90;
+    local padding="";
+    local result="";
 
-    size=${#MESSAGE}
-    paddingSize=`expr ${MAX_SIZE} - ${size}`
+    size=${#MESSAGE};
+    paddingSize=`expr ${MAX_SIZE} - ${size}`;
 
     if [ ${paddingSize} -gt 0 ]; then
         for i in $(seq 0 ${paddingSize})
         do
-            padding+=" "
+            padding+=" ";
         done
     fi
 
     if [ ${EXIT_STATUS} -eq ${EXIT_SUCCESS} ]; then
-        result="[   OK   ]"
+        result="[   OK   ]";
     else
-        result="[ FAILED ]"
+        result="[ FAILED ]";
     fi
 
-    echo "${MESSAGE} ${padding} ${result}"
+    echo "${MESSAGE} ${padding} ${result}";
 }
 
 # Checks the availability of a binary and exits if not available.
@@ -76,24 +76,24 @@ create_containerd_container()
     ns=`sudo ctr namespaces list | grep ${NAMESPACE} | tr -d '[:space:]'`;
     if [ "$ns" != "${NAMESPACE}" ]; then
         sudo ctr namespace create ${NAMESPACE} > /dev/null 2>&1;
-        display_message $? "Creating namespace ${NAMESPACE}"
+        display_message $? "Creating namespace ${NAMESPACE}";
     fi
 
     # Check containerd image name
     in=`sudo ctr -n ${NAMESPACE} images list | grep ${IMAGE_PATH} | awk '{print $1}' | tr -d '[:space:]'`;
     if [ "${in}" != "${IMAGE_PATH}" ]; then
         sudo ctr -n ${NAMESPACE} images pull ${IMAGE_PATH} > /dev/null 2>&1;
-        display_message $? "Pulling image ${IMAGE_PATH} with namespace ${NAMESPACE}"
+        display_message $? "Pulling image ${IMAGE_PATH} with namespace ${NAMESPACE}";
     fi
 
     # Check container name
     cn=`sudo ctr -n ${NAMESPACE} containers list | grep ${CONTAINER_NAME} | awk '{print $1}' | tr -d '[:space:]'`;
     if [ "${cn}" != ${CONTAINER_NAME} ]; then
         sudo ctr -n ${NAMESPACE} container create ${IMAGE_PATH} ${CONTAINER_NAME} > /dev/null 2>&1;
-        display_message $? "Creating container ${CONTAINER_NAME} with namespace ${NAMESPACE}"
+        display_message $? "Creating container ${CONTAINER_NAME} with namespace ${NAMESPACE}";
     fi
 
-    sleep 5
+    sleep 5;
 }
 
 # Create test file entries
@@ -108,15 +108,15 @@ create_test_file_entries()
     sudo mkdir -p ${MOUNT_POINT}/var/lib;
 
     # Create test containers
-    create_containerd_container default docker.io/library/nginx:latest nginx-specimen
-    sleep 2
+    create_containerd_container default docker.io/library/nginx:latest nginx-specimen;
+    sleep 2;
 
-    create_containerd_container dfirlabs docker.io/library/redis:latest redis-specimen
-    sleep 2
+    create_containerd_container dfirlabs docker.io/library/redis:latest redis-specimen;
+    sleep 2;
 
     # Copy containerd files
-    sudo cp -r /var/lib/containerd ${MOUNT_POINT}/var/lib
-    display_message $? "Copying /var/lib/containerd to ${MOUNT_POINT}/var/lib"
+    sudo cp -r /var/lib/containerd ${MOUNT_POINT}/var/lib;
+    display_message $? "Copying /var/lib/containerd to ${MOUNT_POINT}/var/lib";
 }
 
 # Creates a test image file
@@ -136,10 +136,10 @@ create_test_image_file()
     local ARGUMENTS=("$@");
 
     sudo dd if=/dev/zero of=${IMAGE_FILE} bs=${SECTOR_SIZE} count=$(( ${IMAGE_SIZE} / ${SECTOR_SIZE})) 2> /dev/null;
-    display_message $? "Creating disk image ${IMAGE_FILE}"
+    display_message $? "Creating disk image ${IMAGE_FILE}";
 
     sudo mkfs.ext4 -F -q ${ARGUMENTS[@]} ${IMAGE_FILE};
-    display_message $? "Making EXT4 file system (${IMAGE_FILE})"
+    display_message $? "Making EXT4 file system (${IMAGE_FILE})";
 }
 
 # Creates a test image file with file entries
@@ -161,17 +161,17 @@ create_test_image_with_file_entries()
     create_test_image_file ${IMAGE_FILE} ${IMAGE_SIZE} ${SECTOR_SIZE} ${ARGUMENTS[@]};
 
     sudo mount -o loop,rw ${IMAGE_FILE} ${MOUNT_POINT};
-    display_message $? "Mounting ${IMAGE_FILE} to ${MOUNT_POINT} in Read/Write mode"
+    display_message $? "Mounting ${IMAGE_FILE} to ${MOUNT_POINT} in Read/Write mode";
 
     sudo chown ${USERNAME}:${GROUPNAME} ${MOUNT_POINT};
 
     create_test_file_entries ${MOUNT_POINT};
 
     sudo umount ${MOUNT_POINT};
-    display_message $? "Unmounting ${MOUNT_POINT}"
+    display_message $? "Unmounting ${MOUNT_POINT}";
 
     sudo mount -o loop,ro ${IMAGE_FILE} ${MOUNT_POINT};
-    display_message $? "Mounting ${IMAGE_FILE} to ${MOUNT_POINT} in ReadOnly mode"
+    display_message $? "Mounting ${IMAGE_FILE} to ${MOUNT_POINT} in ReadOnly mode";
 }
 
 
@@ -194,7 +194,7 @@ sudo mkdir -p ${MOUNT_POINT};
 
 # Need at least 4 GB
 IMAGE_SIZE=$(( 1 * 1024 * 1024 * 1024 ));
-SECTOR_SIZE=512
+SECTOR_SIZE=512;
 
 # Create a EXT4 file system
 create_test_image_with_file_entries "${SPECIMENS_PATH}/ext4.raw" ${IMAGE_SIZE} ${SECTOR_SIZE} "-L containerd_test";
